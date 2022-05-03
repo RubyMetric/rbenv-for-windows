e3037c5343969991ee144f2eac467d1e74f49a5f
ccmywish <ccmywish@qq.com>
2022-05-03 18:56:10 +0800

    use rbenv versions

--- libexec/rbenv-versions.ps1
+++ libexec/rbenv-versions.ps1
@@ -1,6 +1,17 @@
 # Usage: rbenv versions
 # Summary: List installed Ruby versions
 
-function list_installed_versions {
-
+function all_installed_versions {
+    (Get-ChildItem ($env:RBENV_ROOT)) `
+        # + (Get-ChildItem "$rbenvdir\shims") `
+        | Where-Object { $_.name -match '\d{1,2}\.\d{1,3}\.\d{1,4}.*?$' }
 }
+
+function list_installed_versions {
+    $all_installed_versions = all_installed_versions
+    foreach ($ver in $all_installed_versions) {
+        Write-Host $ver.Name
+    }
+}
+
+list_installed_versions
