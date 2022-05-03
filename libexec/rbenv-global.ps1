9e7b7b1ce07462d28c42ed1f73662967f51686d3
ccmywish <ccmywish@qq.com>
2022-05-03 22:08:37 +0800

    rbenv global basiclly

--- libexec/rbenv-global.ps1
+++ libexec/rbenv-global.ps1
@@ -1,3 +1,31 @@
 # Usage: rbenv global [<version>]
 # Summary: Set or show the global Ruby version
 
+param($cmd)
+
+function get_global_version() {
+    Get-Content $GLOBAL_VERSION_FILE | Out-Host
+}
+
+
+function set_global_version($version) {
+    $shimsdir = "$env:RBENV_ROOT\shims"
+
+    if (Test-Path $shimsdir) {
+        # remove read-only attribute on link
+        # attrib $shimsdir -R /L
+
+        # remove the junction
+        Remove-Item $shimsdir -Recurse -Force -ErrorAction Stop
+    }
+
+    New-Item -Path $shimsdir -ItemType Junction -Value $version | Out-Null
+    $version | Out-File $GLOBAL_VERSION_FILE
+}
+
+
+if (! $cmd) {
+  get_global_version
+} else {
+  set_global_version($cmd)
+}
