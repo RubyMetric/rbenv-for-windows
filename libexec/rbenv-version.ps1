22bfe92428167d2905311f6f9327a97fd82e733c
ccmywish <ccmywish@qq.com>
2022-05-03 17:21:16 +0800

    Handle system ruby

--- libexec/rbenv-version.ps1
+++ libexec/rbenv-version.ps1
@@ -2,7 +2,17 @@
 # Summary: Show the current Ruby version and its origin
 
 function print_global_version {
-  Write-Host $env:RBENV_VERSION_GLOBAL
+    if (! $env:RBENV_VERSION_GLOBAL) {
+        $system_ruby = $(Get-Command ruby)
+        if ($system_ruby) {
+            Write-Host "system(v$($system_ruby.Version)  $($system_ruby.Source))"
+        } else {
+            Write-Host "rbenv: No Ruby installed on your system"
+            Write-Host "       Try use rbenv install <version>"
+        }
+    } else {
+        Write-Host $env:RBENV_VERSION_GLOBAL
+    }
 }
 
 print_global_version
--- libexec/rbenv-versions.ps1
+++ libexec/rbenv-versions.ps1
@@ -1,26 +1,6 @@
 # Usage: rbenv versions
 # Summary: List installed Ruby versions
 
-# ---------------------------------------------------------------
-# File          : rbenv-versions.ps1
-# Authors       : ccmywish <ccmywish@qq.com>
-# Created on    : <2022-05-02>
-# Last modified : <2022-05-02>
-#
-# rbenv-versions:
-#
-#   List installed Ruby versions
-#
-# ----------
-# Changelog:
-#
-# ~> v0.1.0
-# <2022-05-02> Create file
-# ---------------------------------------------------------------
+function list_installed_versions {
 
-<#
-
-Comment:
-
-#>
-
+}
