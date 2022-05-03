2c25bf467c548dfb764dfd97970baf31617fa481
ccmywish <ccmywish@qq.com>
2022-05-04 00:11:20 +0800

    Add versions

--- libexec/rbenv-install.ps1
+++ libexec/rbenv-install.ps1
@@ -1,26 +1,6 @@
 # Usage: rbenv install <version>
 # Summary: Install a Ruby version using RubyInstaller2
 
-# ---------------------------------------------------------------
-# File          : rbenv-install.ps1
-# Authors       : ccmywish <ccmywish@qq.com>
-# Created on    : <2022-05-02>
-# Last modified : <2022-05-02>
-#
-# rbenv-install:
-#
-#   It installs RubyInstaller2 and msys64.
-#
-# ----------
-# Changelog:
-#
-# ~> v0.1.0
-# <2022-05-02> Create file
-# ---------------------------------------------------------------
+function install_msys {
 
-<#
-
-Comment:
-
-#>
-
+}
---
+++ share/README.md
@@ -0,0 +1,10 @@
+# Versions
+
+Sorry for not support x86 versions. If you are interested, you can fork it. Maybe you can be a maintainer, contact me please.
+
+**NOTE**: Versions before `2.0.0-p0`, I don't know either they are x86 or x64.
+
+
+**For maintainers**:
+
+Add from https://rubyinstaller.org/downloads/archives/
