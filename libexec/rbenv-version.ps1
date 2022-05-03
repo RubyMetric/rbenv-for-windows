ae502fbc9873cb933e4e669bb1a57787e1968acb
ccmywish <ccmywish@qq.com>
2022-05-03 17:09:09 +0800

    Finish rbenv version

--- libexec/rbenv-version.ps1
+++ libexec/rbenv-version.ps1
@@ -1,7 +1,8 @@
 # Usage: rbenv version
 # Summary: Show the current Ruby version and its origin
 
-function get_global_version {
+function print_global_version {
   Write-Host $env:RBENV_VERSION_GLOBAL
 }
 
+print_global_version
