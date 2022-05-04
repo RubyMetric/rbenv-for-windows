2ccade535dfea30cd62aa5a4fab8cf473f0d1c4c
ccmywish <ccmywish@qq.com>
2022-05-04 17:04:09 +0800

    Add update command

---
+++ libexec/rbenv-update.ps1
@@ -0,0 +1,28 @@
+# Usage: rbenv update [<rbenv>]
+# Summary: Update rbenv or MSYS2
+# Help: rbenv update          => Update rbenv itself
+# rbenv update rbenv    => Update rbenv itself
+# rbenv update msys     => Update msys2
+# rbenv update msys2    => Same with 'update msys'
+
+param($cmd)
+
+function update_rbenv {
+    git -C $env:RBENV_ROOT\rbenv pull
+}
+
+
+function update_msys2 {
+    ridk exec "pacman -Syu"
+}
+
+
+if (! $cmd) {
+    update_rbenv
+} elseif ($cmd -eq 'rbenv') {
+    update_rbenv
+} elseif ($cmd -eq 'msys' -or $cmd -eq 'msys2' ) {
+    update_msys2
+} else {
+    rbenv help update
+}
