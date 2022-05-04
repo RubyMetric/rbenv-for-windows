3bc8f5cf3837d33329fc9837520215d711fc086a
ccmywish <ccmywish@qq.com>
2022-05-04 15:25:20 +0800

    use scoop download

--- lib/core.ps1
+++ lib/core.ps1
@@ -2,3 +2,27 @@
 function relpath($path) {
     "$($myinvocation.psscriptroot)\$path"
 }
+
+
+# messages
+function abort($msg, [int] $exit_code=1) { write-host $msg -f red; exit $exit_code }
+function error($msg) { write-host "ERROR $msg" -f darkred }
+function warn($msg) {  write-host "WARN  $msg" -f darkyellow }
+function success($msg) { write-host $msg -f darkgreen }
+
+
+function filesize($length) {
+    $gb = [math]::pow(2, 30)
+    $mb = [math]::pow(2, 20)
+    $kb = [math]::pow(2, 10)
+
+    if($length -gt $gb) {
+        "{0:n1} GB" -f ($length / $gb)
+    } elseif($length -gt $mb) {
+        "{0:n1} MB" -f ($length / $mb)
+    } elseif($length -gt $kb) {
+        "{0:n1} KB" -f ($length / $kb)
+    } else {
+        "$($length) B"
+    }
+}
--- libexec/rbenv-install.ps1
+++ libexec/rbenv-install.ps1
@@ -1,6 +1,191 @@
+# rbenv patch to
+#    https://github.com/ScoopInstaller/Scoop/blob/master/lib/install.ps1
+#
+
 # Usage: rbenv install <version>
 # Summary: Install a Ruby version using RubyInstaller2
 
-function install_msys {
 
+# e.g.
+# Scoop/1.0 (+http://scoop.sh/) PowerShell/7.2 (Windows NT 10.0; Win64; x64; Core)
+function Get-UserAgent() {
+    return "Scoop/1.0 (+http://scoop.sh/) PowerShell/$($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor) (Windows NT $([System.Environment]::OSVersion.Version.Major).$([System.Environment]::OSVersion.Version.Minor); $(if($env:PROCESSOR_ARCHITECTURE -eq 'AMD64'){'Win64; x64; '})$(if($env:PROCESSOR_ARCHITEW6432 -eq 'AMD64'){'WOW64; '})$PSEdition)"
+}
+
+
+# paths
+function fname($path) { split-path $path -leaf }
+function strip_ext($fname) { $fname -replace '\.[^\.]*$', '' }
+function strip_filename($path) { $path -replace [regex]::escape((fname $path)) }
+function strip_fragment($url) { $url -replace (new-object uri $url).fragment }
+
+
+function cookie_header($cookies) {
+    if(!$cookies) { return }
+
+    $vals = $cookies.psobject.properties | ForEach-Object {
+        "$($_.name)=$($_.value)"
+    }
+
+    [string]::join(';', $vals)
+}
+
+
+# Used in dl to show progress
+function dl_progress($read, $total, $url) {
+    $console = $host.UI.RawUI;
+    $left  = $console.CursorPosition.X;
+    $top   = $console.CursorPosition.Y;
+    $width = $console.BufferSize.Width;
+
+    if($read -eq 0) {
+        $maxOutputLength = $(dl_progress_output $url 100 $total $console).length
+        if (($left + $maxOutputLength) -gt $width) {
+            # not enough room to print progress on this line
+            # print on new line
+            write-host
+            $left = 0
+            $top  = $top + 1
+            if($top -gt $console.CursorPosition.Y) { $top = $console.CursorPosition.Y }
+        }
+    }
+
+    write-host $(dl_progress_output $url $read $total $console) -nonewline
+    [console]::SetCursorPosition($left, $top)
+}
+
+
+# download with filesize and progress indicator
+function dl($url, $to, $cookies, $progress) {
+    $reqUrl = ($url -split "#")[0]
+    $wreq = [net.webrequest]::create($reqUrl)
+    if($wreq -is [net.httpwebrequest]) {
+        $wreq.useragent = Get-UserAgent
+        if (-not ($url -imatch "sourceforge\.net" -or $url -imatch "portableapps\.com")) {
+            $wreq.referer = strip_filename $url
+        }
+        if($cookies) {
+            $wreq.headers.add('Cookie', (cookie_header $cookies))
+        }
+    }
+
+    try {
+        $wres = $wreq.GetResponse()
+    } catch [System.Net.WebException] {
+        $exc = $_.Exception
+        $handledCodes = @(
+            [System.Net.HttpStatusCode]::MovedPermanently,  # HTTP 301
+            [System.Net.HttpStatusCode]::Found,             # HTTP 302
+            [System.Net.HttpStatusCode]::SeeOther,          # HTTP 303
+            [System.Net.HttpStatusCode]::TemporaryRedirect  # HTTP 307
+        )
+
+        # Only handle redirection codes
+        $redirectRes = $exc.Response
+        if ($handledCodes -notcontains $redirectRes.StatusCode) {
+            throw $exc
+        }
+
+        # Get the new location of the file
+        if ((-not $redirectRes.Headers) -or ($redirectRes.Headers -notcontains 'Location')) {
+            throw $exc
+        }
+
+        $newUrl = $redirectRes.Headers['Location']
+        info "Following redirect to $newUrl..."
+
+        # Handle manual file rename
+        if ($url -like '*#/*') {
+            $null, $postfix = $url -split '#/'
+            $newUrl = "$newUrl#/$postfix"
+        }
+
+        dl $newUrl $to $cookies $progress
+        return
+    }
+
+    $total = $wres.ContentLength
+    # rbenv: we don't use ftp
+    # if($total -eq -1 -and $wreq -is [net.ftpwebrequest]) {
+    #   $total = ftp_file_size($url)
+    # }
+
+    # Define inner function dl_onProgress
+    if ($progress -and ($total -gt 0)) {
+        [console]::CursorVisible = $false
+        function dl_onProgress($read) {
+            dl_progress $read $total $url
+        }
+    } else {
+        write-host "Downloading $url ($(filesize $total))..."
+        function dl_onProgress {
+            #no op
+        }
+    }
+
+    try {
+        $s = $wres.getresponsestream()
+        $fs = [io.file]::openwrite($to)
+        $buffer = new-object byte[] 2048
+        $totalRead = 0
+        $sw = [diagnostics.stopwatch]::StartNew()
+
+        dl_onProgress $totalRead
+        while(($read = $s.read($buffer, 0, $buffer.length)) -gt 0) {
+            $fs.write($buffer, 0, $read)
+            $totalRead += $read
+            if ($sw.elapsedmilliseconds -gt 100) {
+                $sw.restart()
+                dl_onProgress $totalRead
+            }
+        }
+        $sw.stop()
+        dl_onProgress $totalRead
+    } finally {
+        if ($progress) {
+            [console]::CursorVisible = $true
+            write-host
+        }
+        if ($fs) {
+            $fs.close()
+        }
+        if ($s) {
+            $s.close();
+        }
+        $wres.close()
+    }
+}
+
+
+# call-seq:
+# download
+#   dl
+#     dl_onProgress (inner defined)
+#       dl_progress (show progress)
+function download($url, $to, $cookies) {
+    # True of False
+    $progress = [console]::isoutputredirected -eq $false -and
+        $host.name -ne 'Windows PowerShell ISE Host'
+
+    try {
+        dl $url $to $cookies $progress
+    } catch {
+        $e = $_.exception
+        if($e.innerexception) { $e = $e.innerexception }
+        throw $e
+    }
+}
+
+
+# Download the lates x64 msys2
+# Sorry, I don't have time to support x86 version
+# If you want, just fork it
+#
+# Why we don't use scoop to install msys2?
+# I must say, if you directly install via scoop,
+# RubyInstaller will cause more time to find it, that's too bad.
+#
+# We offer the best way to coordinate with RubyInstaller2
+function download_msys2 {
+   $url =  "https://repo.msys2.org/distrib/msys2-x86_64-latest.exe"
 }
