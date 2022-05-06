bd8e2ab8b2b401418cc9921fed13fd3e768b2168
ccmywish <ccmywish@qq.com>
2022-05-06 08:10:03 +0800

    Add rbenv-local set get

--- libexec/rbenv-local.ps1
+++ libexec/rbenv-local.ps1
@@ -1,2 +1,25 @@
 # Usage: rbenv local [<version>]
 # Summary: Set or show the local application-specific Ruby version
+
+param($cmd)
+
+
+# Read the .ruby-version file
+function set_local_version($version) {
+
+    $local_version_file = "$PWD\.ruby-version"
+
+    $version = auto_fix_version_for_installed $version
+
+    Set-Content $local_version_file $version
+}
+
+
+if (!$cmd) {
+    $version = get_local_version
+    if ($version -eq $NULL) {
+        Write-Host "rbenv: no local version configured for this directory"
+    }
+} else {
+    set_local_version($cmd)
+}
