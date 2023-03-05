cce0c8cc107c4a19ff74c08d69c4310cdd30bb0c
Aoran Zeng <ccmywish@qq.com>
2023-03-05 16:50:34 +0800

    Design batch shim

--- tools/build.ps1
+++ tools/build.ps1
@@ -25,15 +25,20 @@
 $dir = "$env:RBENV_ROOT\rbenv"
 
 function build_fake_ruby() {
-    dmd -O -release -inline -of="$dir\bin\ruby.exe" "$dir\source\ruby.d" "$dir\bin\source\rbenv.d"
+    dmd -O -release -inline -of="$dir\bin\ruby.exe" "$dir\source\ruby.d" "$dir\source\rbenv\common.d"
 }
 
 function build_rbenv_rehash() {
-    dmd -O -release -inline -of="$dir\libexec\rbenv-rehash.exe" "$dir\source\rbenv-rehash.d" "$dir\bin\source\rbenv.d"
+    dmd -O -release -inline -of="$dir\libexec\rbenv-rehash.exe" "$dir\source\rbenv-rehash.d" "$dir\source\rbenv\common.d"
 }
 
+function build_rbenv_shim() {
+    dmd -O -release -inline -of="$dir\libexec\rbenv-shim.exe" "$dir\source\rbenv-shim.d" "$dir\source\rbenv\common.d"
+}
 
 Write-Host "rbenv: Build fake ruby.exe to $dir\bin\"
 build_fake_ruby
 Write-Host "rbenv: Build rbenv-rehash.exe to $dir\libexec\"
 build_rbenv_rehash
+Write-Host "rbenv: Build rbenv-shim.exe to $dir\libexec\"
+build_rbenv_shim
