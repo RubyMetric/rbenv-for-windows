0c091f4af891d4e716b61c08be0b3d26b0b9ac6c
Aoran Zeng <ccmywish@qq.com>
2026-01-07 20:37:16 +0800

    Using `ldc2` instead of `dmd`

--- tool/build.ps1
+++ tool/build.ps1
@@ -4,7 +4,7 @@
 # File Name     : build.ps1
 # File Authors  : Aoran Zeng <ccmywish@qq.com>
 # Created On    : <2023-03-04>
-# Last Modified : <2024-09-02>
+# Last Modified : <2026-01-07>
 #
 # build:
 #
@@ -20,41 +20,82 @@
 #       (2) ./build fast
 #
 #       (3) ./build export
+#
+#   You can also change the compiler by using '-compiler dmd' not '-compiler=dmd'
 # ---------------------------------------------------------------
 
 # working dir, is not where this script locates
 # (get-location).path
 
-param($option)
+param($option, $compiler = "ldc2")
 
 $dir = "$env:RBENV_ROOT\rbenv"
 
-function build_fake_ruby($fastmode) {
-    if(!$fastmode) {
-        $flags = '-O', '-release', '-inline'
+$script:compiler_flags = @()
+
+function set_compiler_flags($fast_mode) {
+    if ($compiler -eq "ldc2") {
+        use_ldc2 $fast_mode
     }
-    dmd $flags -of="$dir\bin\ruby.exe" "$dir\source\ruby.d" "$dir\source\rbenv\common.d"
+
+    elseif ($compiler -eq "dmd") {
+        use_dmd $fast_mode
+    }
+
+    else {
+        Write-Host "Unsupported D compiler: $compiler"
+        exit 1
+    }
 }
 
-function build_rbenv_exec($fastmode) {
-    if(!$fastmode) {
-        $flags = '-O', '-release', '-inline';
+function use_dmd($fast_mode) {
+    if (!$fast_mode) {
+        $script:compiler_flags = '-O', '-release', '-inline'
+    } else {
+        $script:compiler_flags = @()
     }
-    dmd $flags -of="$dir\libexec\rbenv-exec.exe" "$dir\source\rbenv-exec.d" "$dir\source\rbenv\common.d"
+}
+
+function use_ldc2($fast_mode) {
+    if (!$fast_mode) {
+        # ldc2's -O is -O3
+        # ldc2 doesn't support '-inline'
+        $script:compiler_flags = '-O', '-release'
+    } else {
+        $script:compiler_flags = @()
+    }
 }
 
 
+function build_fake_ruby() {
+    & $compiler $script:compiler_flags -of="$dir\bin\ruby.exe" "$dir\source\ruby.d" "$dir\source\rbenv\common.d"
+}
+
+function build_rbenv_exec() {
+    & $compiler $script:compiler_flags -of="$dir\libexec\rbenv-exec.exe" "$dir\source\rbenv-exec.d" "$dir\source\rbenv\common.d"
+}
+
+
+# main
 if ($option -eq 'fast') {
-    Write-Host "rbenv: Fast Building fake ruby.exe to $dir\bin\"
-    build_fake_ruby fast
-    Write-Host "rbenv: Fast Building rbenv-exec.exe to $dir\libexec\"
-    build_rbenv_exec fast
+    set_compiler_flags $true
 } else {
-    Write-Host "rbenv: Building fake ruby.exe to $dir\bin\"
-    build_fake_ruby
-    Write-Host "rbenv: Building rbenv-exec.exe to $dir\libexec\"
-    build_rbenv_exec
+    set_compiler_flags $false
 }
+
+Write-Host "rbenv: " -NoNewline
+if ($option -eq 'fast') {
+    Write-Host "(fast mode) " -ForegroundColor Yellow -NoNewline
+}
+Write-Host "Using " -NoNewline
+Write-Host "$compiler" -ForegroundColor Blue -NoNewline
+Write-Host " with " -NoNewline
+Write-Host "$script:compiler_flags" -ForegroundColor Magenta
+
+Write-Host "rbenv: Building fake ruby.exe to $dir\bin\"
+build_fake_ruby
+Write-Host "rbenv: Building rbenv-exec.exe to $dir\libexec\"
+build_rbenv_exec
 
 
 if ($option -eq 'export') {
